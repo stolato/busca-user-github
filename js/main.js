18b64e3c61487642a9c4b391a62f1995d259ab2b
Luiz Carlos <luiz@oppweb.com.br>
2018-05-06 16:08:34 -0300

    Alteração no layout e funções

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ app.controller('Ctrl', function($scope, $http) {
       $('.loading').show();
       $http.get("https://api.github.com/search/users?q="+script)
       .then(function (response) {$scope.users = response.data.items;}).finally(function(){ $('.loading').hide(); });
+      resize();
     };
     $scope.detail = function(section){
         $http.get(section.url).then(function (data) {
@@ -20,3 +21,18 @@ app.controller('Ctrl', function($scope, $http) {
       $('#exampleModal').modal('show');
     };
 });
+
+
+function resize(){
+  setTimeout(function(){
+    var t = 100;
+    $('.card').each(function(){
+        if($(this).height() > t){
+          t = $(this).height();
+        }
+        console.log($(this).height());
+        console.log(t);
+    });
+    $('.card').height(t);
+  },500);
+}
